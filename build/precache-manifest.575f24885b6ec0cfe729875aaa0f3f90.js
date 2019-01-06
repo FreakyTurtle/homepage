3698172d7f294a599d4324c64c10c7064cb816c0
FreakyTurtle <jagues@gmail.com>
2019-01-06 01:07:21 +0000

    new files

---
+++ build/precache-manifest.575f24885b6ec0cfe729875aaa0f3f90.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f41ad4ad48e65b705f65",
+    "url": "/static/css/main.c805d681.chunk.css"
+  },
+  {
+    "revision": "4b7c37ae3ef77bd91f8d",
+    "url": "/static/js/1.4b7c37ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f41ad4ad48e65b705f65",
+    "url": "/static/js/main.f41ad4ad.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d71657facf3a551425cac970cbffff63",
+    "url": "/index.html"
+  }
+];
